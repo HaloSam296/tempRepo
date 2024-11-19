a0a82c6119bab7e9b9f0c32015eae0d8baec4463
HaloSam296 <halosam296@gmail.com>
2024-11-19 01:02:38 -0600

    Update dvwaSetup.sh

--- dvwaSetup.sh
+++ dvwaSetup.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Update package index
 sudo apt update
 
 # Install necessary packages
-sudo apt install -y apache2 mysql-server php php-gd php-mysql php-curl php-mbstring php-xml php-xmlrpc git
+sudo apt install -y software-properties-common git
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update
+sudo apt install -y apache2 mysql-server php8.1 php8.1-gd php8.1-mysql php8.1-curl php8.1-mbstring php8.1-xml php8.1-xmlrpc
 
 # Secure MySQL installation
 sudo mysql_secure_installation
 
+# Prompt for database password
+read -sp "Enter a password for the DVWA database user: " DB_PASSWORD
+echo
+
 # Create DVWA database and user
 sudo mysql -u root -p <<MYSQL_SCRIPT
 CREATE DATABASE dvwadb;
-CREATE USER 'dvwa'@'localhost' IDENTIFIED BY 'dvwapassword';
+CREATE USER 'dvwa'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON dvwadb.* TO 'dvwa'@'localhost';
 FLUSH PRIVILEGES;
 EXIT;
 MYSQL_SCRIPT
+
 
 # Download DVWA
 cd /var/www/html
@@ -32,15 +42,13 @@ sudo cp /var/www/html/DVWA/config/config.inc.php.dist /var/www/html/DVWA/config/
 sudo sed -i "s/$_DVWA\['db_server'\] = 'localhost';/$_DVWA\['db_server'\] = '127.0.0.1';/" /var/www/html/DVWA/config/config.inc.php
 sudo sed -i "s/$_DVWA\['db_database'\] = 'dvwa';/$_DVWA\['db_database'\] = 'dvwadb';/" /var/www/html/DVWA/config/config.inc.php
 sudo sed -i "s/$_DVWA\['db_user'\] = 'root';/$_DVWA\['db_user'\] = 'dvwa';/" /var/www/html/DVWA/config/config.inc.php
-sudo sed -i "s/$_DVWA\['db_password'\] = 'p@ssw0rd';/$_DVWA\['db_password'\] = 'dvwapassword';/" /var/www/html/DVWA/config/config.inc.php
+sudo sed -i "s/$_DVWA\['db_password'\] = 'p@ssw0rd';/$_DVWA\['db_password'\] = '$DB_PASSWORD';/" /var/www/html/DVWA/config/config.inc.php
 
-# Configure PHP
-sudo sed -i "s/allow_url_include = Off/allow_url_include = On/" /etc/php/7.2/apache2/php.ini
 
-# Configure Apache
 sudo sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/DVWA|" /etc/apache2/sites-enabled/000-default.conf
 
-# Restart Apache
+# Restart services
 sudo systemctl restart apache2
+sudo systemctl restart mysql
 
 echo "DVWA installation completed. Access it at http://localhost"
